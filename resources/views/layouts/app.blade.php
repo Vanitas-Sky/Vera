c6aeb57ae7734dd6e3545d37145fac931f23a92c
Vanitas <230672519+Vanitas-Sky@users.noreply.github.com>
2026-08-07 09:35:12 -0600

    favicon
    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>{{ config('app.name', 'Vera') }}</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Favicon del Diamante de Vera -->
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
